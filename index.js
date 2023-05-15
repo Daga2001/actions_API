4d98489e9435d9d06f2329f807fb4165c202a428
Daga2001 <dav1dguxman@hotmail.com>
2023-05-15 10:50:07 -0500

    test

--- index.js
+++ index.js
@@ -8,4 +8,4 @@ function resta(a, b) {
 
 console.log("La suma de 3 y 4 es:", suma(3, 4));
 console.log("La resta de 7 y 2 es:", resta(7, 2));
-console.log("new action!")
+console.log("new action!");
